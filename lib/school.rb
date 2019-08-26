2cf1423889561e87f4f487e42fb412b4b74e6f85
Brad Alexander <brad.alexander913@gmail.com>
2019-08-26 22:46:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 class School 
   
-  def initialize(name)
-    @name = name 
+  def initialize(school_name)
+    @school_name = school_name 
     @roster = {} 
   end 
   
@@ -9,6 +9,7 @@ class School
     @roster 
   end
   
-   def add_student(name,grade)
+   def add_student(student_name, grade)
+     roster[student_name] => grade
   end
 end
